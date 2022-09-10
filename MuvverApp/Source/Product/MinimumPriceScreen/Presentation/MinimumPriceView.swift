9fcbfc93a61fa4d2bedf87b15c18676cc2af3c81
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-09-09 22:20:16 -0300

    feature: Material.io slider

--- MuvverApp/Source/Product/MinimumPriceScreen/Presentation/MinimumPriceView.swift
+++ MuvverApp/Source/Product/MinimumPriceScreen/Presentation/MinimumPriceView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import MaterialComponents.MaterialSlider
 
 class MinimumPriceView: UIView {
     //MARK: - Components
@@ -17,24 +18,39 @@ class MinimumPriceView: UIView {
         return grad
     }()
     
-    lazy var tableViewLabel: UILabel = {
+    lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Preço de entrega"
         label.font = UIFont.boldSystemFont(ofSize: 20)
         return label
     }()
+    
+    lazy var priceSlider: MDCSlider = {
+        let slider = MDCSlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.addTarget(self, action: #selector(didChangeSliderValue(senderSlider:)), for: .valueChanged)
+        slider.color = .black
+        return slider
+    }()
     //MARK: - Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
         addSubview(blackTopGradient)
-        addSubview(tableViewLabel)
+        addSubview(titleLabel)
+        addSubview(priceSlider)
         configConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Objc functions
+    
+    @objc func didChangeSliderValue(senderSlider:MDCSlider) {
+      print("Did change slider value to: \(senderSlider.value)")
     }
     
     //MARK: - Constraints
@@ -46,8 +62,12 @@ class MinimumPriceView: UIView {
             blackTopGradient.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             blackTopGradient.bottomAnchor.constraint(equalTo: self.topAnchor, constant: 165),
             
-            tableViewLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 18),
-            tableViewLabel.topAnchor.constraint(equalTo: blackTopGradient.bottomAnchor, constant: 25),
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 18),
+            titleLabel.topAnchor.constraint(equalTo: blackTopGradient.bottomAnchor, constant: 25),
+            
+            priceSlider.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 15),
+            priceSlider.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            priceSlider.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -18),
         ])
         
     }
